f038b51f6ee8ac081aad8a41ea46e484a331b315
Alexander Nesterenko <nestof250@gmail.com>
2016-03-15 18:09:07 +0200

    Init commit for lab2

---
+++ lab2/classifier.py
@@ -0,0 +1,21 @@
+"""
+Classifier class.
+
+Based on simple range-based recogition.
+"""
+
+
+class Classifier():
+    """Class that implements dummy classifier."""
+
+    def __init__(self, training_data):
+        """Initialize classifier with training data."""
+        self.train(training_data)
+
+    def train(self, data):
+        """Train classifier."""
+        # TODO:
+
+    def classify(self, data):
+        """Find class of @data."""
+        # TODO
